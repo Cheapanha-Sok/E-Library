701e1a1213ffbf7c979158e4be7053a044da18b0
Cheapanha-Sok <Cheapanha-Sok>
2024-01-23 15:23:25 +0700

    updated

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,12 +5,13 @@ import { getFirestore   } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDlCzQw0jACqDXO2291PKuEgTOEb84B1Wo",
-  authDomain: "project-website-e-library.firebaseapp.com",
-  projectId: "project-website-e-library",
-  storageBucket: "project-website-e-library.appspot.com",
-  messagingSenderId: "962962738093",
-  appId: "1:962962738093:web:c24fb69d1060d6ba942f24",
+  // Put ur firebase right here
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
